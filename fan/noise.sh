3be29fe3aca5cd323249ceaf1abbfb20583d2a0f
diggerdu <diggerdu@hotmail.com>
2017-07-10 00:28:05 +0800

    toad

---
+++ fan/noise.sh
@@ -0,0 +1,16 @@
+su -c "echo 1 > /sys/class/hwmon/hwmon1/pwm1_enable"
+su -c "echo 120 > /sys/class/hwmon/hwmon1/pwm1"
+su -c "echo 1 > /sys/class/hwmon/hwmon1/pwm2_enable"
+su -c "echo 255 > /sys/class/hwmon/hwmon1/pwm2"
+su -c "echo 1 > /sys/class/hwmon/hwmon1/pwm3_enable"
+su -c "echo 120 > /sys/class/hwmon/hwmon1/pwm3"
+su -c "echo 1 > /sys/class/hwmon/hwmon1/pwm4_enable"
+su -c "echo 120 > /sys/class/hwmon/hwmon1/pwm4"
+
+nvidia-settings -a "[gpu:0]/GPUFanControlState=1"
+nvidia-settings -a "[gpu:1]/GPUFanControlState=1"
+nvidia-settings -a "[gpu:2]/GPUFanControlState=1"
+nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=90"
+nvidia-settings -a "[fan:1]/GPUTargetFanSpeed=90"
+nvidia-settings -a "[fan:2]/GPUTargetFanSpeed=90"
+
